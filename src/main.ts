be58a0729165206e86dc6bc635d6242c23436918
Amr Eldokany <a.eldokany@source.org>
2026-05-22 11:00:38 +0300

    fix: enable cors for vercel frontend

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://fleetfuelpro.vercel.app',
+      'https://fleet-fuel-9kuwwpqyl-aeldokany86-gifs-projects.vercel.app',
+    ],
     credentials: true,
   });
 
