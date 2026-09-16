5f6a15647568e429b28ce0ce88a641d31fe652f9
Amr Eldokany <a.eldokany86@gmail.com>
2026-09-16 23:44:36 +0300

    Fix employee transfers service export

--- src/employee-transfers/employee-transfers.module.ts
+++ src/employee-transfers/employee-transfers.module.ts
@@ -6,6 +6,7 @@ import { NotificationsModule } from '../notifications/notifications.module';
 @Module({
   imports: [NotificationsModule],
   controllers: [EmployeeTransfersController],
-  providers: [EmployeeTransfersService]
+  providers: [EmployeeTransfersService],
+  exports: [EmployeeTransfersService],
 })
 export class EmployeeTransfersModule {}
